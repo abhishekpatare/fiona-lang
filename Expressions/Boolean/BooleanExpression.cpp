d7bacd2748808b4b1e38e0b74ce899b5ff23ea32
SanidhyaDighiya <dighiya_sr@cs.iitr.ac.in>
2023-02-16 22:28:34 +0530

    Impemented Boolean Expression

--- Expressions/Boolean/BooleanExpression.cpp
+++ Expressions/Boolean/BooleanExpression.cpp
@@ -1,7 +1,60 @@
 #include "BooleanExpression.h"
 #include "../expression.h"
+#include "SymTabEntry.h"
 
-class BooleanExpression:public ExpressionNode{
+class BooleanExpression:public ExpressionNode<bool>{
     public:
     virtual bool get_value()=0;
 };
+
+//BooleanLiteralNode
+bool BooleanLiteralNode::get_value(){
+    return this->value;
+}
+
+
+//BooleanConstantNode
+bool BooleanConstantNode::get_value(){
+    // return this->value;
+    //todo
+}
+
+
+//BooleanVariableNode
+sid BooleanVariableNode::get_sid(){
+    return this->id;
+}
+
+bool BooleanVariableNode::get_value(){
+    //todo
+}
+
+
+//BooleanOrNode
+bool BooleanOrNode::get_value(){
+    bool left = this->left->get_value();
+    bool right =  this->right->get_value();
+    return (left||right);
+}
+
+
+//BooleanAndNode
+bool BooleanAndNode::get_value(){
+    bool left = this->left->get_value();
+    bool right =  this->right->get_value();
+    return (left&&right);
+}
+
+
+//BooleanXorNode
+bool BooleanXorNode::get_value(){
+    bool left = this->left->get_value();
+    bool right =  this->right->get_value();
+    return (left^right);
+}
+
+
+//BooleanNotNode
+bool BooleanNotNode::get_value(){
+    return (!this->value);
+}
--- Expressions/Boolean/BooleanExpression.h
+++ Expressions/Boolean/BooleanExpression.h
@@ -2,36 +2,58 @@
 #ifndef BOOLEANEXPRESSION_NODE
 #define BOOLEANEXPRESSION_NODE
 
-class BooleanExpression:public ExpressionNode{
+class BooleanExpression:public ExpressionNode<bool>{
+    
+};
+
+class BooleanLiteralNode:public BooleanExpression{
+    bool value;
     public:
-    virtual bool get_value()=0;
+    bool get_value();
 };
 
-class BooleanLiteralNode:public ExpressionNode{
-
+class BooleanConstantNode:public BooleanExpression{
+    const sid id;
+    public:
+    bool get_value();
+};
+class BooleanVariableNode:public BooleanExpression{
+    bool value;
+    sid id;
+    public:
+    sid get_sid();
+    bool get_value();
 };
 
-class BooleanConstantNode:public ExpressionNode{
-
-};
-class BooleanVariableNode:public ExpressionNode{
-
+class BooleanOperationNode:public BooleanExpression{
+    protected:
+    BooleanExpression *left, *right;
 };
 
-class BooleanOrNode:public ExpressionNode{
+class BooleanOrNode:public BooleanOperationNode{
 
+    public:
+    // BooleanExpression *left, *right;
+    bool get_value();
 };
 
-class BooleanAndNode:public ExpressionNode{
-
+class BooleanAndNode:public BooleanOperationNode{
+    public:
+    // BooleanExpression *left, *right;
+    bool get_value();
 };
 
-class BooleanXorNode:public ExpressionNode{
-
+class BooleanXorNode:public BooleanOperationNode{
+    public:
+    // BooleanExpression *left, *right;
+    bool get_value();
 };
 
-class BooleanNotNode:public ExpressionNode{
-
+class BooleanNotNode:public BooleanExpression{
+    bool value;
+    public:
+    // BooleanExpression *left, *right;
+    bool get_value();
 };
 
 #endif
