f6d92bdc345aea02193863118523e5ffae0ce15f
SanidhyaDighiya <dighiya_sr@cs.iitr.ac.in>
2023-02-16 21:09:51 +0530

    changed Block Node

--- Statements/Loops.h
+++ Statements/Loops.h
@@ -1,12 +1,16 @@
 #include "statement.h"
+#include "BlockNode.h"
+#include "../Expressions/Boolean/BooleanExpression.h"
 #ifndef LOOP_NODE
 #define LOOP_NODE
 
 class LoopNode:public StatementNode{
-
+    BlockNode *blk;
+    public:
+        void execute();
 };
 class WhileNode:public StatementNode{
-
+    bool cond;
 };
 class ForNode:public StatementNode{
 
